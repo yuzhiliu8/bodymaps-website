1cdd2df10fc52fbb0e45b3b63b8f8e9f58f0e6c6
yuzhiliu8 <137526570+yuzhiliu8@users.noreply.github.com>
2024-07-05 11:25:09 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react'
+import "./App.css"
+
+export default function App() {
+
+  const [data, setData] = useState();
+
+
+  function test(){
+    console.log('test');
+    setData('test');
+    const url = "https://jsonplaceholder.typicode.com/todos/1";
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setData(JSON.stringify(data));
+    })
+  }
+
+
+  // useEffect(() => {
+  //   const url = "https://www.reddit.com/r/javascript/top/.json?limit=5";
+  //   fetch(url)
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //     setData(data);
+  //   })
+
+  // })
+
+  return (
+    <div className="App">
+      <div className="head">
+        My App
+      </div>
+
+      <button onClick={() => test()}>
+        Get
+      </button>
+
+      <div className="data-output">
+        {data}
+      </div>
+    </div>    
+  )
+}
