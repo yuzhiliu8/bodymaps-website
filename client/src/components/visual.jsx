ecd91b99fb94bbc8a4b49b53c7b58ffa0b28bc8c
yuzhiliu8 <137526570+yuzhiliu8@users.noreply.github.com>
2024-07-31 17:00:39 -0400

    added windowLevel, zoom, and mousewheel tools

--- client/src/components/visual.jsx
+++ client/src/components/visual.jsx
@@ -10,11 +10,12 @@ function Visual({ niftiURL, filename }) {
   const axial_ref = useRef(null);
   const sagittal_ref = useRef(null);
   const coronal_ref = useRef(null);
+  const acq_ref = useRef(null);
   
   useEffect(() => {              //TODO: Need to not make a new renderingEngine on every render. 
     if (axial_ref && sagittal_ref && coronal_ref){
       console.log("setup");  
-      setup(axial_ref, sagittal_ref, coronal_ref, niftiURL);
+      setup(axial_ref, sagittal_ref, coronal_ref, acq_ref, niftiURL);
     }  
     }, [])
  
@@ -24,10 +25,7 @@ function Visual({ niftiURL, filename }) {
           <div className="axial" ref={axial_ref} style={{height: "50vh", width: "39.5vw"}}></div>
           <div className="sagittal" ref={sagittal_ref}></div>
           <div className="coronal" ref={coronal_ref}></div>
-          <div className="tools">
-            <div>Tools</div>
-            <div>Showing: {filename} </div>
-          </div>
+          <div className="tools" ref={acq_ref}></div>
         </div>
     </div>
   )
--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -5,69 +5,125 @@ import {
     volumeLoader,
     setVolumesForViewports,
   } from '@cornerstonejs/core';
-import { init as csTools3dInit } from '@cornerstonejs/tools';
+import {
+  init as csTools3dInit,
+  ZoomTool,
+  WindowLevelTool,
+  StackScrollMouseWheelTool,
+  ToolGroupManager,
+  addTool,
+  Enums as csToolsEnums
+}from '@cornerstonejs/tools';
+
 import { cornerstoneNiftiImageVolumeLoader } from '@cornerstonejs/nifti-volume-loader';
 
-export async function setup(ref1, ref2, ref3, niftiURL){
-    const viewportId1 = 'CT_NIFTI_AXIAL';
-    const viewportId2 = 'CT_NIFTI_SAGITTAL';
-    const viewportId3 = 'CT_NIFTI_CORONAL';
+export async function setup(ref1, ref2, ref3, ref4, niftiURL){
+  await csTools3dInit()
+  ref1.current.oncontextmenu = (e) => e.preventDefault();
+  ref2.current.oncontextmenu = (e) => e.preventDefault();
+  ref3.current.oncontextmenu = (e) => e.preventDefault();
+  ref4.current.oncontextmenu = (e) => e.preventDefault();
 
-    volumeLoader.registerVolumeLoader('nifti', cornerstoneNiftiImageVolumeLoader);
-    
-    const volumeId = 'nifti:' + niftiURL;
 
-    const volume = await volumeLoader.createAndCacheVolume(volumeId);
+  const viewportId1 = 'CT_NIFTI_AXIAL';
+  const viewportId2 = 'CT_NIFTI_SAGITTAL';
+  const viewportId3 = 'CT_NIFTI_CORONAL';
+  const viewportId4 = 'CT_NIFTI_ACQUISITION';
 
-    const renderingEngineId = 'myRenderingEngine1';
-    const renderingEngine = new RenderingEngine(renderingEngineId);
-    
-    
-    const viewportInputArray = [
-        {
-          viewportId: viewportId1, 
-          type: Enums.ViewportType.ORTHOGRAPHIC,
-          element: ref1.current,
-          defaultOptions: {
-            orientation: Enums.OrientationAxis.AXIAL,
-          },
+  volumeLoader.registerVolumeLoader('nifti', cornerstoneNiftiImageVolumeLoader);
+  
+  const volumeId = 'nifti:' + niftiURL;
+  const volume = await volumeLoader.createAndCacheVolume(volumeId);
+
+  const renderingEngineId = 'myRenderingEngine';
+  const renderingEngine = new RenderingEngine(renderingEngineId);
+
+  const toolGroupId = "myToolGroup";
+  const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+
+  addTool(ZoomTool);
+  addTool(WindowLevelTool);
+  addTool(StackScrollMouseWheelTool)
+
+  toolGroup.addTool(ZoomTool.toolName)
+  toolGroup.addTool(WindowLevelTool.toolName)
+  toolGroup.addTool(StackScrollMouseWheelTool.toolName)
+  toolGroup.addViewport(viewportId1, renderingEngineId)
+  toolGroup.addViewport(viewportId2, renderingEngineId)
+  toolGroup.addViewport(viewportId3, renderingEngineId)
+  toolGroup.addViewport(viewportId4, renderingEngineId)
+
+  toolGroup.setToolActive(StackScrollMouseWheelTool.toolName);
+  toolGroup.setToolActive(WindowLevelTool.toolName, {
+    bindings: [
+      {
+        mouseButton: csToolsEnums.MouseBindings.Primary, // Left Click
+      },
+    ],
+  });
+
+  toolGroup.setToolActive(ZoomTool.toolName, {
+    bindings: [
+      {
+        mouseButton: csToolsEnums.MouseBindings.Secondary, // Right Click
+      },
+    ],
+  });
+  
+  
+  const viewportInputArray = [
+      {
+        viewportId: viewportId1, 
+        type: Enums.ViewportType.ORTHOGRAPHIC,
+        element: ref1.current,
+        defaultOptions: {
+          orientation: Enums.OrientationAxis.AXIAL,
         },
-        {
-          viewportId: viewportId2,
-          type: Enums.ViewportType.ORTHOGRAPHIC,
-          element: ref2.current,
-          defaultOptions: {
-            orientation: Enums.OrientationAxis.SAGITTAL,
-          },
+      },
+      {
+        viewportId: viewportId2,
+        type: Enums.ViewportType.ORTHOGRAPHIC,
+        element: ref2.current,
+        defaultOptions: {
+          orientation: Enums.OrientationAxis.SAGITTAL,
         },
-        {
-          viewportId: viewportId3,
-          type: Enums.ViewportType.ORTHOGRAPHIC,
-          element: ref3.current, 
-          defaultOptions: {
-            orientation: Enums.OrientationAxis.CORONAL,
-          },
+      },
+      {
+        viewportId: viewportId3,
+        type: Enums.ViewportType.ORTHOGRAPHIC,
+        element: ref3.current, 
+        defaultOptions: {
+          orientation: Enums.OrientationAxis.CORONAL,
         },
-      ];
+      },
+      {
+        viewportId: viewportId4,
+        type: Enums.ViewportType.ORTHOGRAPHIC,
+        element: ref4.current,
+        defaultOptions:{
+          orientation: Enums.OrientationAxis.ACQUISITION
+        },
+      }
+    ];
 
-    renderingEngine.setViewports(viewportInputArray);
+  renderingEngine.setViewports(viewportInputArray);
 
-    setVolumesForViewports(
-        renderingEngine,
-        [{ volumeId }],
-        viewportInputArray.map((v) => v.viewportId)
-    );
-    renderingEngine.getViewports().map((v) => {
-      v.canvas.style.position = "relative";
-    })
+  setVolumesForViewports(
+      renderingEngine,
+      [{ volumeId }],
+      viewportInputArray.map((v) => v.viewportId)
+  );
+  renderingEngine.getViewports().map((v) => {
+    v.canvas.style.position = "relative";
+  })
 
-    renderingEngine.render();
-    
-    
-    
-    // vp1.resize()
+  renderingEngine.render();
+  
+  
+  
+  // vp1.resize()
 
-    return renderingEngine;
+  return renderingEngine;
 }
 
 export async function initializeCornerstone(){
