61e2aef1c2bcb13cb223b390cf8949c6e07de836
yuzhiliu8 <137526570+yuzhiliu8@users.noreply.github.com>
2024-08-02 14:05:15 -0400

    get contour points w/ opencv

--- flask-server/handle.py
+++ flask-server/handle.py
@@ -1,67 +1,9 @@
 import nibabel as nib
 import matplotlib.pyplot as plt
-import numpy as np
 from decimal import Decimal
-import time
 
-path = 'nifti/ct.nii.gz'
+
+path = 'nifti/aorta.nii.gz'
 img = nib.load(path)
 data = img.get_fdata()
-
-def visualize(data):
-    x = data.shape[0] // 2
-    # x = 0
-    y = data.shape[1] // 2
-    # z = data.shape[2] // 2
-    z = 0
-
-
-    # Create a figure with three subplots
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
-
-    # Display the middle slices
-    # ax1.imshow(data[x , :, :], cmap='gray')
-    # ax1.set_title(f'Sagittal View: Slice {x} of {data.shape[0]}')
-    # ax1.axis('off')
-
-    # ax2.imshow(data[:, y, :], cmap='gray')
-    # ax2.set_title(f'Coronal View: Slice {y} of {data.shape[1]}')
-    # ax2.axis('off')
-
-    plt.ion()
-
-    for z in range(data.shape[2]):
-        ax3.imshow(data[:, :, z], cmap='gray')
-        ax3.set_title(f'Axial View: Slice {z} of {data.shape[2]}')
-        ax3.axis('off')
-
-        # plt.tight_layout()
-        plt.draw()
-        plt.pause(1)
-        plt.clf()
-    
-    plt.ioff()
-    plt.show()
-
-
-def save_as_text(data, output_path):
-    """
-    Save the data as a string in a text file.
-    
-    :param data: 3D numpy array of image data
-    :param output_path: Path to save the text file
-    """
-    with open(output_path, 'w') as f:
-        # Write shape information
-        f.write(f"Shape: {data.shape}\n\n")
-        
-        # Write data values
-        for i in range(data.shape[0]):
-            f.write(f"Slice {i}:\n")
-            np.savetxt(f, data[i], fmt='%.2f', delimiter=', ')
-            f.write('\n')
-    
-    print(f"Data saved as text to {output_path}")
-
-# save_as_text(data, 'data.txt')
-visualize(data)
+print(data[:, :, 0].shape)
--- flask-server/processMasks.py
+++ flask-server/processMasks.py
@@ -1,2 +1,37 @@
-import cv2
+import cv2 as cv
 import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+
+def getContours(filepath):
+    aorta = nib.load(filepath)
+    aorta_data = aorta.get_fdata()    #aorta_data[x, y, z]
+
+    #AXIAL ORIENTATION (Z axis)
+    slice_index = aorta_data.shape[2] //2
+    aorta_slice = aorta_data[:, :, slice_index]
+    # aorta_slice = cv.transpose(aorta_slice)
+
+    aorta_slice = cv.normalize(aorta_slice, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U) #normalize aorta slice = CV_8U
+    print(aorta_slice.shape)
+
+    contours, _ = cv.findContours(aorta_slice, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    aorta_slice2 = aorta_data[:, :, slice_index]
+    for contour in contours:
+        for point in contour:
+            x, y = point[0]
+            aorta_slice2[y, x] = 1000
+
+
+    
+
+
+    cv.imshow('orig', aorta_slice2)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+
+filepath = './nifti/aorta.nii.gz'
+getContours(filepath)
