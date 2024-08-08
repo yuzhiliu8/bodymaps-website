f02dab3cdfc16534678f4235f0226ba1b1cb0f43
yuzhiliu8 <137526570+yuzhiliu8@users.noreply.github.com>
2024-08-07 20:09:39 -0400

    gh pgs

--- client/src/routes/Test.jsx
+++ client/src/routes/Test.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { useRef, useEffect } from 'react';
-import Visual from '../components/Visual'
 import './Test.css';
 
-import NestedCheckBox from '../components/NestedCheckBox';
 
 function Test() {
     return (
-        <NestedCheckBox />
+        <div>Test</div>
     )
 }
 
