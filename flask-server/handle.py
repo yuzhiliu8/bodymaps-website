c72cc7e5cb1d8810895929e378560b76e6345ba3
yuzhiliu8 <yuzhiliu8@gmail.com>
2024-08-26 23:47:01 -0400

    binary erosion 4 pixels

--- flask-server/handle.py
+++ flask-server/handle.py
@@ -1,12 +1,19 @@
 import nibabel as nib
 import numpy as np
-from constants import organ_ids, reportScreenNames
+from constants import organ_ids, reportScreenNames, organ_ids_volumeNA
+import scipy.ndimage as ndimage
+
+EROSION_PIXELS = 4
+cube_len = (2 * EROSION_PIXELS) + 1
+structuring_element = np.ones([cube_len, cube_len, cube_len], dtype=bool)
 
 def voxelThreshold(slice):
     num_voxels = len(slice[slice > 0])
     return num_voxels < 100
 
-def isComplete(img_data):
+def getCalcVolumeState(img_data, organ):
+    if organ in organ_ids_volumeNA:
+        return "NA" # blood vessel, volume is NA
     slices = [
         img_data[:, :, 0],
         img_data[:, :, -1],
@@ -17,8 +24,8 @@ def isComplete(img_data):
     ]
     for slice in slices:
         if voxelThreshold(slice) is False:
-            return False
-    return True
+            return "incomplete"
+    return "complete"
 
 
 def processMasks(serverDir):
@@ -29,15 +36,23 @@ def processMasks(serverDir):
         organ_data['id'] = reportScreenNames[i]
         img = nib.load(f'{serverDir}/segmentations/{organ_ids[i]}.nii.gz')
         img_data = img.get_fdata()
-        if (isComplete(img_data)):
+        state = getCalcVolumeState(img_data, organ_ids[i])
+        if state == "complete":
             volume_cm = round(float(nib.imagestats.mask_volume(img)/1000), 2)
             organ_data['volume_cm'] = volume_cm
-        else:
-            organ_data['volume_cm'] = 'incomplete organ'
+        elif state == "incomplete":
+            organ_data['volume_cm'] = "Incomplete organ"
+        elif state == "NA":
+            organ_data['volume_cm'] = "N/A"
         
-        hu_values = ct[img_data > 0]
+        erosion_data = ndimage.binary_erosion(img_data, structure=structuring_element)
+        hu_values = ct[erosion_data > 0]
+        # hu_values = ct[img_data > 0]
         mean_hu = round(float(np.mean(hu_values)), 2)
-        organ_data['mean_hu'] = mean_hu
+        if np.isnan(mean_hu):
+            organ_data['mean_hu'] = 'N/A'
+        else:
+            organ_data['mean_hu'] = mean_hu 
         data['data'].append(organ_data)
         
     return data
