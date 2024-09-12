ad293616780ccb04bfa5eb2be8b68817d9ccecb3
yuzhiliu8 <yuzhiliu8@gmail.com>
2024-09-12 19:03:30 -0400

    env basename

--- flask-server/constants.py
+++ flask-server/constants.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_PATH = os.environ['BASE_PATH']
 reportScreenNames = ['Aorta', 'Gallbladder', 'Kidney (L)', 'Kidney (R)', 'Liver', 'Pancreas', 'Postcava', 'Spleen', 'Stomach']
-organ_ids_volumeNA = ('aorta', 'postcava')
+organ_ids_volumeNA = ('aorta.nii.gz', 'postcava.nii.gz')
 main_nifti_filename = 'ct.nii.gz'
